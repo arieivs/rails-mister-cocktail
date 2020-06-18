86eca23c5f9c3b67fcbd992438f2beb05ff65112
Sara Vieira <s.cvieira@sapo.pt>
2020-06-19 00:43:55 +0100

    mrs cocktail day 1 complete

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,8 @@
 class DosesController < ApplicationController
-  def new
-    @dose = Dose.new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-  end
+  # def new
+  #   @dose = Dose.new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  # end
 
   def create
     @dose = Dose.new(dose_params)
@@ -17,7 +17,9 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
